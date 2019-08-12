2b076127f7b84cc3488abe478f0c69e609b7fe7f
DESKTOP-AUB9LAC\takuy <mokekedasho@icloud.com>
2019-08-12 23:40:53 +0900

    add RequestClass

--- core/ClassLoader.php
+++ core/ClassLoader.php
@@ -23,10 +23,12 @@
          */
         public function loadClass($class){
             foreach ($this->dirs as $dir){
-                $file = $dir . ''. $class . '.php';
-                require $file;
+                $file = $dir . '/'. $class . '.php';
+                if(is_readable($file)){
+                    require $file;
 
-                return;
+                    return;
+                }
             }
         }
     }
